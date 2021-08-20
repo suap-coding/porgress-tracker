1284b2b40a58783770571721f60d965a178de9d9
suap <suap.coding@gmail.com>
2021-08-21 00:08:52 +0200

    Stupid, but works.

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <time.h>
 #include <vector>
+#include <conio.h>
+#include <Windows.h>
 
 #include "Task.h"
 
@@ -8,15 +10,21 @@ int main()
 {
     using namespace std;
 
-    //clock_t begin, end;
-    //Task t(clock());
+
+    //char c = 3;
+    //while (!_kbhit()) {
+    //    c = _getch();
+    //    cout << c;
+    //}
 
     vector<Task> tasks;
 
     // main loop
     bool proceed = true;
-    while (proceed)
+    while (proceed )
     {
+        cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+
         int taskNr = 1;
         int currentlyActive = 0;
         for (auto& task : tasks) {
@@ -38,7 +46,11 @@ int main()
             << " f - to finish current task,\n"
             << " q - to close program.\n";
         char controlChar;
-        cin >> controlChar;
+        controlChar = 'Z';
+        if (_kbhit()) {
+            controlChar = _getch();
+        }
+        //cin >> controlChar;
 
         string name;
         switch (controlChar)
@@ -65,10 +77,22 @@ int main()
             proceed = false;
             break;
         // TODO: check if number was picked!
-        default:
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
             if(currentlyActive > 0) tasks[currentlyActive - 1].deactivate();
             tasks[controlChar - 1 - '0'].activate();
+            break;
+        default:
+            break;
         }
-        cout << endl;
+
+        Sleep(333);
     }
 }
